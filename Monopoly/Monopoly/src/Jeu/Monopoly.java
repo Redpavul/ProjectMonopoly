ecc9348b1653443840199abe3d779837aca3ef06
nerokiryu <nero.kiryu@gmail.com>
2015-05-19 10:55:40 +0200

    test

--- Monopoly/Monopoly/src/Jeu/Monopoly.java
+++ Monopoly/Monopoly/src/Jeu/Monopoly.java
@@ -9,11 +9,14 @@ import java.util.ArrayList;
 public class  Monopoly {
     
     
+
 	private Carreau carreaux;
 	private Joueur[] joueurs;
 	private Interface interf;
 	private int nbMaisons = 32;
 	private int nbHotels = 12;
+	private int des1 ;
+	private int des2 ;
     
 	
 	public Monopoly(String dataFilename)
@@ -140,6 +143,66 @@ public class  Monopoly {
 			System.err.println("[buildGamePlateau()] : Error while reading file!");
 		}
 	
+	}
+	public void roll(){
+		setDes1((int)(Math.random()*6)+1);
+		setDes2((int)(Math.random()*6)+1);
+	}
+
+	public Carreau getCarreaux() {
+		return carreaux;
+	}
+
+	public void setCarreaux(Carreau carreaux) {
+		this.carreaux = carreaux;
+	}
+
+	public Joueur[] getJoueurs() {
+		return joueurs;
+	}
+
+	public void setJoueurs(Joueur[] joueurs) {
+		this.joueurs = joueurs;
+	}
+
+	public Interface getInterf() {
+		return interf;
+	}
+
+	public void setInterf(Interface interf) {
+		this.interf = interf;
+	}
+
+	public int getNbMaisons() {
+		return nbMaisons;
+	}
+
+	public void setNbMaisons(int nbMaisons) {
+		this.nbMaisons = nbMaisons;
+	}
+
+	public int getNbHotels() {
+		return nbHotels;
+	}
+
+	public void setNbHotels(int nbHotels) {
+		this.nbHotels = nbHotels;
+	}
+
+	public int getDes1() {
+		return des1;
+	}
+
+	private void setDes1(int des1) {
+		this.des1 = des1;
+	}
+
+	public int getDes2() {
+		return des2;
+	}
+
+	private void setDes2(int des2) {
+		this.des2 = des2;
 	}
 
 			
