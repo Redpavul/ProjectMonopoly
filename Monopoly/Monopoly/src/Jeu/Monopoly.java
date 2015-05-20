52bdf560545432bed49c47ac6da4b7b5d4a8d376
redpavul <redpavul@redpavul-K73SD>
2015-05-20 20:16:22 +0200

    Monopoly.java modifié

--- Monopoly/Monopoly/src/Jeu/Monopoly.java
+++ Monopoly/Monopoly/src/Jeu/Monopoly.java
@@ -10,11 +10,13 @@ public class  Monopoly {
     
     
 
-	private Carreau carreaux;
 	private Joueur[] joueurs;
 	private Interface interf;
 	private int nbMaisons = 32;
 	private int nbHotels = 12;
+	private HashMap<String, Groupe> listGroupes = new HashMap();
+	private Groupe g;
+	private Carreau [] listeCarreaux;
 	private int des1 ;
 	private int des2 ;
     
@@ -22,11 +24,18 @@ public class  Monopoly {
 	public Monopoly(String dataFilename)
 	{
 		buildGamePlateau(dataFilename);
-		buildGroupe(dataFilename);
 	}
 	
 	private void buildGamePlateau(String dataFilename)
 	{
+		
+		for(CouleurPropriete c : CouleurPropriete.values())
+		{
+			System.out.println("Le groupe de couleur "+c.toString()+" a bien été créée");
+			g = new Groupe(new ArrayList<ProprieteAConstruire>(), c);
+			listGroupes.put(c.toString(), g);
+			
+		}
 		try{
 			ArrayList<String[]> data = readDataFile(dataFilename, ",");
 			
@@ -35,6 +44,19 @@ public class  Monopoly {
 				String caseType = data.get(i)[0];
 				if(caseType.compareTo("P") == 0){
 					System.out.println("Propriété :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+					
+					int[] loyers = {Integer.parseInt(data.get(i)[5]), Integer.parseInt(data.get(i)[6]), 
+								Integer.parseInt(data.get(i)[7]), Integer.parseInt(data.get(i)[8]),
+								Integer.parseInt(data.get(i)[9]),Integer.parseInt(data.get(i)[10])};
+					int prixMaison = Integer.parseInt(data.get(i)[11]);
+					int prixHotel = Integer.parseInt(data.get(i)[12]);
+					Groupe g = listGroupes.get(data.get(i)[3]);
+					String nomCarreau = data.get(i)[2];
+					int numeroCarreau = Integer.parseInt(data.get(i)[1]);
+					int prixAchat = Integer.parseInt(data.get(i)[4]);
+					ProprieteAConstruire p = new ProprieteAConstruire(prixAchat, nomCarreau, numeroCarreau, g, loyers, prixMaison, prixHotel);
+					System.out.println(nomCarreau);
+					
 				}
 				else if(caseType.compareTo("G") == 0){
 					System.out.println("Gare :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
@@ -54,6 +76,7 @@ public class  Monopoly {
 				else
 					System.err.println("[buildGamePleateau()] : Invalid Data type");
 			}
+			
 			
 		} 
 		catch(FileNotFoundException e){
@@ -79,83 +102,13 @@ public class  Monopoly {
 		return data;
 	}
 	
-	private void buildGroupe(String dataFilename)
-	{
-		
-
-		
-		for(CouleurPropriete c : CouleurPropriete.values())
-		{
-			System.out.println("Le groupe de couleur "+c.toString()+" a bien été créée");
-			Groupe a = new Groupe(new ArrayList<ProprieteAConstruire>(),c);
-		}
-		
-		try{
-			ArrayList<String[]> data = readDataFile(dataFilename, ",");
-			
 	
-			for(int i=0; i<data.size(); ++i){
-				String caseType = data.get(i)[0];
-				if(caseType.compareTo("P") == 0){
-					String caseCoul = data.get(i)[3];
-					if(caseCoul.compareTo("mauve")==0)
-					{
-						System.out.println("Couleur de groupe :\t" + data.get(i)[3]);
-					}
-					else if (caseCoul.compareTo("bleuCiel")==0)
-					{
-						System.out.println("Couleur de groupe :\t" + data.get(i)[3]);
-					}
-					else if (caseCoul.compareTo("violet")==0)
-					{
-						System.out.println("Couleur de groupe :\t" + data.get(i)[3]);
-					}
-					else if (caseCoul.compareTo("orange")==0)
-					{
-						System.out.println("Couleur de groupe :\t" + data.get(i)[3]);
-					}
-					else if (caseCoul.compareTo("rouge")==0)
-					{
-						System.out.println("Couleur de groupe :\t" + data.get(i)[3]);
-					}
-					else if (caseCoul.compareTo("jaune")==0)
-					{
-						System.out.println("Couleur de groupe :\t" + data.get(i)[3]);
-					}
-					else if (caseCoul.compareTo("vert")==0)
-					{
-						System.out.println("Couleur de groupe :\t" + data.get(i)[3]);
-					}
-					else if (caseCoul.compareTo("bleuFoncé")==0)
-					{
-						System.out.println("Couleur de groupe :\t" + data.get(i)[3]);
-					}
-				}
-				else ;
-					//System.err.println("[buildGamePleateau()] : Invalid Data type");
-				}
-			
-		} 
-		catch(FileNotFoundException e){
-			System.err.println("[buildGamePlateau()] : File is not found!");
-		}
-		catch(IOException e){
-			System.err.println("[buildGamePlateau()] : Error while reading file!");
-		}
-	
-	}
 	public void roll(){
 		setDes1((int)(Math.random()*6)+1);
 		setDes2((int)(Math.random()*6)+1);
 	}
 
-	public Carreau getCarreaux() {
-		return carreaux;
-	}
-
-	public void setCarreaux(Carreau carreaux) {
-		this.carreaux = carreaux;
-	}
+	
 
 	public Joueur[] getJoueurs() {
 		return joueurs;
