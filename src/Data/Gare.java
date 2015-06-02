4f17d159d21dfc322888ef36fe0130cb9813ef85
Mattthedark <mattthedark.extinction@gmail.com>
2015-06-02 08:51:02 +0200

    prixAchat

--- src/Data/Gare.java
+++ src/Data/Gare.java
@@ -6,5 +6,6 @@ public class Gare extends CarreauPropriete
 	public Gare(int prixAchat, String nomCarreau, int numeroCarreau)
 	{
 		super(prixAchat, nomCarreau, numeroCarreau);
+                prixAchat = 200;
 	}
 }
