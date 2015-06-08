617a48dd51b97b71ad97e04619277ba7f5100531
CONGIO-HOLLARD Jorane <congiohj@hostname.iut2.upmf-grenoble.fr>
2015-06-08 08:12:37 +0200

    Tour de jeu en cours de developpement, il faut gérer le cas d'une compagnie ou gare

--- src/Data/CouleurPropriete.java
+++ src/Data/CouleurPropriete.java
@@ -3,13 +3,13 @@ package Data;
 public enum CouleurPropriete
 {
 	bleuFonce("bleuFonce"), 
-	orange("orange"), 
+	rouge("rouge"), 
 	mauve("mauve"), 
-	violet("violet"), 
+	vert("vert"), 
 	bleuCiel("bleuCiel"), 
 	jaune("jaune"), 
-	vert("vert"), 
-	rouge("rouge");
+	violet("violet"), 
+	orange("orange");
 	
     private final String label;
     
@@ -17,5 +17,25 @@ public enum CouleurPropriete
         this.label = label; 
     }
 	
-	public String toString() { return this.label; }   
+    public String toString()
+    {
+	String couleur = "";
+	if(this == bleuFonce)
+	    couleur = "34";
+	else if(this == orange)
+	    couleur = "33";
+	else if(this == mauve)
+	    couleur = "35";
+	else if(this == violet)
+	    couleur = "35";
+	else if(this == bleuCiel)
+	    couleur = "36";
+	else if(this == jaune)
+	    couleur = "33";
+	else if(this == vert)
+	    couleur = "32";
+	else if(this == rouge)
+	    couleur = "31";
+	 return "\033[" + couleur + "m" + super.toString() + "\033[0m";
+    }   
 }
