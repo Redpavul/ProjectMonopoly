3e9100097d3d4e1cd96951dc68d59c04264f0733
Mattthedark <mattthedark.extinction@gmail.com>
2015-06-08 14:21:47 +0200

    Calcul loyer gare UP

--- src/Data/Gare.java
+++ src/Data/Gare.java
@@ -8,4 +8,16 @@ public class Gare extends CarreauPropriete
 		super(prixAchat, nomCarreau, numeroCarreau);
                 prixAchat = 200;
 	}
+	
+	public void calculLoyerGare() {
+		Joueur jProprio = this.getProprietaire();
+		int nb = jProprio.getGares().length;
+		int p = nb * 50;
+		System.out.println(jProprio + " est proprietaire de " + nb + " gares, vous devez donc lui payer " + p + " unitées");
+		Joueur j = this.getMonopoly().getJoueurs().getFirst();
+		int c = j.getCash();
+		int total = c - p;
+		System.out.println("Il vous restera " + total + " unitées");
+		j.getMonopoly().payer(j, p, jProprio); // loyer est inclus
+	}
 }
