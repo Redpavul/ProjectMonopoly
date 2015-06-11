0be03c1a06c372d73e97acb24c24618a8246c0c9
73fabien73 <fabien73.lalande@yahoo.fr>
2015-06-11 10:34:36 +0200

    carteTirage fini

--- src/Data/CarreauTirage.java
+++ src/Data/CarreauTirage.java
@@ -11,7 +11,7 @@ public class CarreauTirage extends CarreauAction {
 
     public void action() {
 	String nom = getNomCarreau();
-	if (nom.equalsIgnoreCase("non")) {
+	if (nom.equalsIgnoreCase("Chance")) {
 	    while (!effetChance(getMonopoly().getPositionChance())) {
 		getMonopoly().setPositionChance(getMonopoly().getPositionChance() + 1);
 			if (getMonopoly().getPositionChance() > getMonopoly().getNbDeCarteChance()) {
@@ -114,7 +114,7 @@ public class CarreauTirage extends CarreauAction {
 	    System.out.println("Vous recevez une carte sortie de Prison");
 	    Joueur j = getMonopoly().getJoueurs().getFirst();
 	    j.setCarteSortieDePrison(j.getCarteSortieDePrison() + 1);
-	    System.out.println("vous �tes lib�r� de prison. Cette carte peut �tre conserv�e jusqu'� ce qu'elle soit utilis�e.");
+	    System.out.println("vous etes libere de prison. Cette carte peut etre conservee jusqu'a ce qu'elle soit utilisee.");
 	    getMonopoly().setCarteSortieDePrisonChance(false);
 	    return true;
 	} else {
@@ -138,9 +138,9 @@ public class CarreauTirage extends CarreauAction {
 	}
 
     public void effetChance3() {
-	System.out.println("Vous �tes impos�s pour les r�parations de voirie � raison de :");
-	System.out.println("  -40� par maison");
-	System.out.println("  -115� par h�tel");
+	System.out.println("Vous etes imposes pour les reparations de voirie a raison de :");
+	System.out.println("  -40€ par maison");
+	System.out.println("  -115€ par hotel");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	int nbMaison = 0;
 	int nbHotel = 0;
@@ -150,20 +150,20 @@ public class CarreauTirage extends CarreauAction {
 	}
 	int aPayer = nbMaison * 40 + nbHotel * 115;
 	System.out.println("");
-	System.out.println("Vous devez payer " + aPayer + "�");
+	System.out.println("Vous devez payer " + aPayer + "€");
 	j.setCash(j.getCash() - aPayer);
     }
 
     public void effetChance4() {
-	System.out.println("Amende pour exc�s de vitesse 15�");
+	System.out.println("Amende pour exces de vitesse 15€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() - 15);
     }
 
     public void effetChance5() {
-	System.out.println("Faites des r�parations :");
-	System.out.println("  -25� par maison");
-	System.out.println("  -100� par h�tel");
+	System.out.println("Faites des reparations :");
+	System.out.println("  -25€ par maison");
+	System.out.println("  -100€ par hotel");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	int nbMaison = 0;
 	int nbHotel = 0;
@@ -173,99 +173,111 @@ public class CarreauTirage extends CarreauAction {
 	}
 	int aPayer = nbMaison * 25 + nbHotel * 100;
 	System.out.println("");
-	System.out.println("Vous devez payer " + aPayer + "�");
+	System.out.println("Vous devez payer " + aPayer + "€");
 	j.setCash(j.getCash() - aPayer);
     }
 
     public void effetChance6() {
-	System.out.println("Amende pour ivresse 20�");
+	System.out.println("Amende pour ivresse 20€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() - 20);
     }
 
     public void effetChance7() {
-	System.out.println("Avancer jusqu'� la case D�part");
+	System.out.println("Avancer jusqu'a la case Depart");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setPositionCourante(getMonopoly().getListCarreaux()[40 - j.getPositionCourante().getNumeroCarreau()]);
     }
 
     public void effetChance8() {
-	System.out.println("Aller en prison, ne passez pas par la case D�part");
+	System.out.println("Aller en prison, ne passez pas par la case Depart");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setPrison(true);//fonction qui fait avancer un joueur directement � la case d�part.
     }
 
     public void effetChance9() {
-	System.out.println("Rendez-vous � l'Avenue Henri-Martin");
+	System.out.println("Rendez-vous à l'Avenue Henri-Martin");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	if (25 - j.getPositionCourante().getNumeroCarreau() > 0) {
 	    j.setPositionCourante(getMonopoly().getListCarreaux()[25 - j.getPositionCourante().getNumeroCarreau()]);
 			} else {
 	    j.setPositionCourante(getMonopoly().getListCarreaux()[40 + 25 - j.getPositionCourante().getNumeroCarreau()]);
+		System.out.println("Vous passez par la case dépar et recevez 200€");
+		j.setCash(j.getCash() + 200);
 			}
     }
 
     public void effetChance10() {
-	System.out.println("Rendez-vous � la gare de Lyon");
+	System.out.println("Rendez-vous à la gare de Lyon");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	if (16 - j.getPositionCourante().getNumeroCarreau() > 0) {
 	    j.setPositionCourante(getMonopoly().getListCarreaux()[16 - j.getPositionCourante().getNumeroCarreau()]);
 			} else {
 	    j.setPositionCourante(getMonopoly().getListCarreaux()[40 + 16 - j.getPositionCourante().getNumeroCarreau()]);
+		System.out.println("Vous passez par la case dépar et recevez 200€");
+		j.setCash(j.getCash() + 200);
 			}
     }
 
     public void effetChance11() {
-	System.out.println("Payer pour frais de scolarit� 150�");
+	System.out.println("Payer pour frais de scolarite 150e");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() - 150);
     }
 
     public void effetChance12() {
-	System.out.println("Vous avez gagnez le prix de mots crois�s. Recevez 100�");
+	System.out.println("Vous avez gagnez le prix de mots croises. Recevez 100e");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 100);
     }
 
     public void effetChance13() {
-	System.out.println("La Banque vous verse un dividende de 50�");
+	System.out.println("La Banque vous verse un dividende de 50€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 50);
     }
 
     public void effetChance14() {
-	System.out.println("Rendez-vous � la Rue de la Paix.");
+	System.out.println("Rendez-vous a la Rue de la Paix.");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	if (39 - j.getPositionCourante().getNumeroCarreau() > 0) {
 	    j.setPositionCourante(getMonopoly().getListCarreaux()[39 - j.getPositionCourante().getNumeroCarreau()]);
 			} else {
 	    j.setPositionCourante(getMonopoly().getListCarreaux()[40 + 39 - j.getPositionCourante().getNumeroCarreau()]);
+		System.out.println("Vous passez par la case dépar et recevez 200€");
+		j.setCash(j.getCash() + 200);
 			}
     }
 
     public void effetChance15() {
-	System.out.println("Votre immeuble et votre pr�t rapportent. Vous devez toucher 150�");
+	System.out.println("Votre immeuble et votre pret rapportent. Vous devez toucher 150€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 150);
     }
 
     public void effetChance16() {
-	System.out.println("Acc�dez au Boulevard de la Villette.");
+	System.out.println("Accedez au Boulevard de la Villette.");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	if (12 - j.getPositionCourante().getNumeroCarreau() > 0) {
 	    j.setPositionCourante(getMonopoly().getListCarreaux()[12 - j.getPositionCourante().getNumeroCarreau()]);
 			} else {
 	    j.setPositionCourante(getMonopoly().getListCarreaux()[40 + 12 - j.getPositionCourante().getNumeroCarreau()]);
+		System.out.println("Vous passez par la case dépar et recevez 200€");
+		j.setCash(j.getCash() + 200);
 			}
 
     }
 
+    
+    
+    
+    
     public boolean effetCaisse1() {
 	if (getMonopoly().getCarteSortieDePrisonCaisse()) {
 	    System.out.println("Vous recevez une carte sortie de Prison");
 	    Joueur j = getMonopoly().getJoueurs().getFirst();
 	    j.setCarteSortieDePrison(j.getCarteSortieDePrison() + 1);
-	    System.out.println("vous �tes lib�r� de prison. Cette carte peut �tre conserv�e jusqu'� ce qu'elle soit utilis�e.");
+	    System.out.println("vous etes libere de prison. Cette carte peut etre conservee jusqu'a ce qu'elle soit utilisee.");
 	    getMonopoly().setCarteSortieDePrisonCaisse(false);
 	    return true;
 	} else {
@@ -275,15 +287,14 @@ public class CarreauTirage extends CarreauAction {
     }
 
     public void effetCaisse2() {
-	System.out.println("Payer une amende de 10�");
+	System.out.println("Payer une amende de 10€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() - 10);
     }
 
     public void effetCaisse3() {
-	System.out.println("C'est votre anniversaire, chaque joueur doit vous donner 10�");
+	System.out.println("C'est votre anniversaire, chaque joueur doit vous donner 10€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
-	Monopoly m = getMonopoly();
 	for (Joueur joueur : getMonopoly().getJoueurs()) {
 	    if (joueur != j) {
 		int arg=joueur.getCash();
@@ -299,13 +310,13 @@ public class CarreauTirage extends CarreauAction {
     }
 
     public void effetCaisse4() {
-	System.out.println("Erreur de la banque en votre faveur, recevez 200�");
+	System.out.println("Erreur de la banque en votre faveur, recevez 200€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 200);
     }
 
     public void effetCaisse5() {
-	System.out.println("Retournez � Belleville");
+	System.out.println("Retournez a Belleville");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	if (2 - j.getPositionCourante().getNumeroCarreau() > 0) {
 		j.setPositionCourante(getMonopoly().getListCarreaux()[2 - j.getPositionCourante().getNumeroCarreau()]);
@@ -315,67 +326,67 @@ public class CarreauTirage extends CarreauAction {
     }
 
     public void effetCaisse6() {
-	System.out.println("Payez la note du m�decin 50�");
+	System.out.println("Payez la note du medecin 50€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() - 50);
     }
 
     public void effetCaisse7() {
-	System.out.println("Les contributions vous remboursent la somme de 20�");
+	System.out.println("Les contributions vous remboursent la somme de 20€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 20);
     }
 
     public void effetCaisse8() {
-	System.out.println("Payez � l'h�pital 100�");
+	System.out.println("Payez a l'hopital 100€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() - 100);
     }
 
     public void effetCaisse9() {
-	System.out.println("Vous h�ritez : 100�");
+	System.out.println("Vous heritez : 100€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 20);
     }
 
     public void effetCaisse10() {
-	System.out.println("Aller en prison, ne passez pas par la case D�part");
+	System.out.println("Aller en prison, ne passez pas par la case Depart");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setPrison(true);//fonction qui fait avancer un joueur directement � la case d�part.
     }
 
     public void effetCaisse11() {
-	System.out.println("Payer votre Police d'Assurance : 50�");
+	System.out.println("Payer votre Police d'Assurance : 50€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() - 50);
     }
 
     public void effetCaisse12() {
-	System.out.println("La vente de votre stock vous rapporte 50�");
+	System.out.println("La vente de votre stock vous rapporte 50€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 50);
     }
 
     public void effetCaisse13() {
-	System.out.println("Avancer jusqu'� la case D�part");
+	System.out.println("Avancer jusqu'a la case Depart");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setPositionCourante(getMonopoly().getListCarreaux()[40 - j.getPositionCourante().getNumeroCarreau()]);
     }
 
     public void effetCaisse14() {
-	System.out.println("Recevez votre int�r�t sur l'emprunt � 7% : 25�");
+	System.out.println("Recevez votre interet sur l'emprunt a 7% : 25€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 25);
     }
 
     public void effetCaisse15() {
-	System.out.println("Recevez votre revenu annuel : 100�");
+	System.out.println("Recevez votre revenu annuel : 100€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 100);
     }
 
     public void effetCaisse16() {
-	System.out.println("Vous avez gagn� le deuxi�me prix de beaut�: recevez 10�");
+	System.out.println("Vous avez gagne le deuxieme prix de beaute: recevez 10€");
 	Joueur j = getMonopoly().getJoueurs().getFirst();
 	j.setCash(j.getCash() + 10);
     }
